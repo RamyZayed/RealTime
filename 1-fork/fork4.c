3be41f47d95c6ce2f87393866f2dd92a88d0967c
RamyZayed <rami1112@windowslive.com>
2022-04-21 10:13:33 +0300

    a

--- 1-fork/fork4.c
+++ 1-fork/fork4.c
@@ -10,28 +10,33 @@ int main()
 {
   int i, status;
   pid_t pid, pid_array[3];
-  
+
   printf("My process ID is %d\n", getpid());
-  
-  for ( i = 0; i < 3; i++ ) {
+
+  for (i = 0; i < 3; i++)
+  {
     pid = fork();
-    
-    if ( pid == 0 ) {
+
+    if (pid == 0)
+    {
       printf("I am the child  => PID = %d\n", getpid());
-      while(1);
+      while (1);
     }
-    else {
+    else
+    {
       printf("I am the parent => PID = %d, child ID = %d\n", getpid(), pid);
       pid_array[i] = pid;
     }
   }
-  //while( 1 ) {
-    for ( i = 0; i < 3; i++ ) {
-      if (pid = wait(&status)) {
-	printf("Process ID %d has terminated\n", pid);
-      }
+  // while( 1 ) {
+  for (i = 0; i < 3; i++)
+  {
+    if (pid = wait(&status))
+    {
+      printf("Process ID %d has terminated\n", pid);
     }
+  }
   //}
-  
-  return(0);
+
+  return (0);
 }
